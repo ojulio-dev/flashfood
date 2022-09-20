4be6764b6361866c8db8f2998224618b4303dd19
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-09-20 15:20:12 -0300

    Add Database backup

--- dashboard/config/config.php
+++ dashboard/config/config.php
@@ -3,7 +3,7 @@
 if (ENVIRONMENT === 'development') {
     define('DB_HOST', 'localhost');
     define('DB_PORT', '3306');
-    define('DB_NAME', 'db_flash_food');
+    define('DB_NAME', 'db_flashfood');
     define('DB_USER', 'root');
     define('DB_PASS', '');
 } else {
--- dashboard/models/db_flashfood.sql
+++ dashboard/models/db_flashfood.sql
@@ -7,11 +7,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE DATABASE IF NOT EXISTS `db_flash_food` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-USE `db_flash_food`;
+CREATE DATABASE IF NOT EXISTS `db_flashfood` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `db_flashfood`;
 
-CREATE TABLE IF NOT EXISTS `item` (
-  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
   `category_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `banner` varchar(255) NOT NULL,
@@ -22,11 +22,18 @@ CREATE TABLE IF NOT EXISTS `item` (
   `status` tinyint(4) NOT NULL DEFAULT 1,
   `updated_at` datetime NOT NULL DEFAULT current_timestamp(),
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`item_id`)
+  PRIMARY KEY (`product_id`) USING BTREE,
+  KEY `FK_product_product_category` (`category_id`),
+  CONSTRAINT `FK_product_product_category` FOREIGN KEY (`category_id`) REFERENCES `product_category` (`category_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+INSERT INTO `product` (`product_id`, `category_id`, `name`, `banner`, `description`, `special_price`, `price`, `slug`, `status`, `updated_at`, `created_at`) VALUES
+	(4, 1, 'Sorvete Ovomaltine', '4.jpg', 'O melhor Sorvete do Mundo', 23.00, 32.00, 'sorvete-ovomaltine', 1, '2022-09-17 18:42:27', '2022-09-17 18:42:27'),
+	(6, 3, 'Batata Frita', '6.jpg', 'Melhor Batata Frita do Mundo', 32.32, 23.00, 'batata-frita', 1, '2022-09-17 21:56:29', '2022-09-17 21:56:29'),
+	(12, 4, 'Coca-Cola', '12.jpg', 'Melhor Coca-Cola da Cidade', 4.99, 5.99, 'coca-cola', 0, '2022-09-17 23:22:23', '2022-09-17 23:22:23'),
+	(13, 2, 'Hamburguer Maneiro', '13.jpg', 'Melhor Hamburguer da Cidade', 18.99, 23.99, 'hamburguer-maneiro', 1, '2022-09-17 23:30:10', '2022-09-17 23:30:10');
 
-CREATE TABLE IF NOT EXISTS `item_category` (
+CREATE TABLE IF NOT EXISTS `product_category` (
   `category_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `slug` varchar(50) NOT NULL,
@@ -36,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `item_category` (
   PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `item_category` (`category_id`, `name`, `slug`, `status`, `updated_at`, `created_at`) VALUES
+INSERT INTO `product_category` (`category_id`, `name`, `slug`, `status`, `updated_at`, `created_at`) VALUES
 	(1, 'Sorvete', 'sorvete', 1, '2022-09-17 17:02:47', '2022-09-17 17:02:47'),
 	(2, 'Hamburguer', 'hamburguer', 1, '2022-09-17 17:07:24', '2022-09-17 17:07:24'),
 	(3, 'Salgado', 'salgado', 1, '2022-09-17 21:42:04', '2022-09-17 21:42:04'),
