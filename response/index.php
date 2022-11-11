8a404363f0f4ae61b17260ff0bb3d73e26655511
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2022-11-10 22:28:16 -0300

    Finish header to response

--- response/index.php
+++ response/index.php
@@ -29,7 +29,10 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'main';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- CSS -->
+    <link rel="stylesheet" href="<?= DIR_CSS ?>/reset.css">
+
     <link rel="stylesheet" href="<?= DIR_CSS ?>/style.css">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <?php if (file_exists(__DIR__ . '/assets/css/' . $page . '/style.css')): ?>
--- response/pages/cardapio/produtoDoCardapio.php
+++ response/pages/cardapio/produtoDoCardapio.php
@@ -1,3 +1,89 @@
-<section>
-    
+<section class="sistema-cardapio-wrapper">
+    <div class="sistema-info-wrapper">
+        <div class="titulo-cardapio-wrappera">
+            <img src="<?= DIR_IMG ?>/left.png" alt="">
+            <h2>Big Burguer</h2>
+        </div>
+
+        <div>
+            <img src="" alt="">
+            <div>
+                <strong></strong>
+                <small></small>
+            </div>
+            a
+        </div>
+
+        <div>
+            <h3>Adicionais</h3>
+            <ul>
+                <li>
+                    <p>Hamburguer</p>
+                    <div>
+                        <small>R$ 4,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+                <li>
+                <p>Queijo Prato</p>
+                    <div>
+                        <small>R$ 2,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+                <li>
+                <p>Queijo Cheedar</p>
+                    <div>
+                        <small>R$ 3,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+                <li>
+                <p>Cebola</p>
+                    <div>
+                        <small>R$ 2,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+                <li>
+                <p>Tomate</p>
+                    <div>
+                        <small>R$ 2,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+                <li>
+                <p>Molho Barbecue</p>
+                    <div>
+                        <small>R$ 3,00</small>
+                        <form action="">
+                            <input type="text" name="" id="">
+                        </form>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <div>
+                <form action="">
+                    <input type="text">
+                </form>
+            </div>
+            <div>
+                <i></i>
+                <a href=""></a>
+            </div>
+        </div>
+        <button>Adicionar ao Carrinho (R$ 36,59)</button>
+    </div>
 </section>
