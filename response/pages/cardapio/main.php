0ca33ae4a308f1ecd847d962c1a4d36b0de6f87b
Gustavo Cavaretto <gustavocavaretto@gmail.com>
2022-11-10 21:24:31 -0300

    cardapio iniciado

---
+++ response/pages/cardapio/main.php
@@ -0,0 +1,16 @@
+<section>
+    <div class="menu-opcoes">
+        <div class="opcoes" id="o1">
+            <h3>Proções</h3>
+        </div>
+        <div class="opcoes" id="o2">
+            <h3>Pizzas</h3>
+        </div>
+        <div class="opcoes" id="o3">
+            <h3>Sobremesas</h3>
+        </div>
+        <div class="opcoes" id="o4">
+            <h3>Bebidas</h3>
+        </div>
+    </div>
+</section>
