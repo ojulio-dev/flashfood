f24bc82051f61e539bd57556b96dd32c4e6321e4
Gustavo Cavaretto <gustavocavaretto@gmail.com>
2022-11-30 19:23:44 -0300

    mudanças cardapio

--- mobile/pages/cardapio/main.php
+++ mobile/pages/cardapio/main.php
@@ -51,4 +51,21 @@
             </div>
         </div>
     </div>
+
+    <div id="itens-cardapio">
+        <div class="fundo-produto">
+            <h2 class="main-titulo-produto">Janta do Julião</h2>
+            <div class="img-produto">
+                <img src="<?= DIR_IMG ?>/cardapio/juliaojanta.png" alt="">
+            </div>
+            <div class="especificacoes-produto">
+                <div class="texto-produto">
+                    <img src="<?= DIR_IMG ?>/header/logo-responsivo.png" alt="" class="logo-especificacao"><h4>Pé de galinha angolana, mangericão, moyo shoyo.</h4>
+                </div>
+                <div class="preco-produto">
+                    <h5>R$ 5,50</h5>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
