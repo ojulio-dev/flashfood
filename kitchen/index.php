b1adbda5d7aa7a56689a4c50f3290b71ee85f988
MateusMAART <101677318+MatteusMart@users.noreply.github.com>
2023-01-25 22:28:46 -0300

    add kitchen dashboard

--- kitchen/index.php
+++ kitchen/index.php
@@ -30,11 +30,24 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'main';
 
     <!-- CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="<?= DIR_CSS ?>/reset.css">
+
     <link rel="stylesheet" href="<?= DIR_CSS ?>/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
 </head>
 <body>
     <div id="container">
+        <header id="header-kitchen">
+            <ul>
+                <a href="?page=orders"><li>Pedidos</li></a>
+            </ul>
+        </header>
+
         <?php
 
             if (file_exists(__DIR__ . "/pages/$page/$action.php")) {
---
+++ kitchen/pages/orders/main.php
@@ -0,0 +1,71 @@
+<section>
+    <div id="title">
+        <h2>Pedidos</h2>
+    </div>
+    <div class="main-orders">
+        <div class="elements">
+            <div class="order-info">
+                <div class="position-number-wrapper">
+                    <span>4</span>
+
+                    <h4>#00003</h4>
+                </div>
+                <div class="user-time-wrapper">
+                    <p><i class="fa-solid fa-user"></i> Julio</p>
+                    <span><i class="fa-regular fa-clock"></i> 11m 2s</span>
+                </div>
+            </div>
+
+            <div class="products-wrapper">
+                <ul>
+                    <li>
+                        <h5><span>2</span> Hamburguer</h5>
+
+                        <ul class="additionals-wrapper">
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Molho Branco</small>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Hamburguer</small>
+                            </li>
+                        </ul>
+
+                        <span>*Remover Alface</span>
+                    </li>
+                    <li>
+                        <h5><span>2</span> Hamburguer</h5>
+
+                        <ul class="additionals-wrapper">
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Molho Branco</small>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Hamburguer</small>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h5><span>2</span> Hamburguer</h5>
+
+                        <ul class="additionals-wrapper">
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Molho Branco</small>
+                            </li>
+                            <li>
+                                <i class="fa-solid fa-plus"></i>
+                                <small>Hamburguer</small>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <button>Começar</button>
+        </div>
+    </div>
+</section>
