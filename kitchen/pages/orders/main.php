050233d6a2bceed762386b0249a44c65ab456944
GustavoCavaretto <101677775+GustavoCavaretto@users.noreply.github.com>
2023-01-27 18:05:53 -0300

    cozinha css finalizado

--- kitchen/pages/orders/main.php
+++ kitchen/pages/orders/main.php
@@ -19,53 +19,121 @@
             <div class="products-wrapper">
                 <ul>
                     <li>
-                        <h5><span>2</span> Hamburguer</h5>
+                        <div class="order-wrapper">
+                            <h5><span>2</span> Hamburguer</h5>
 
-                        <ul class="additionals-wrapper">
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Molho Branco</small>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Hamburguer</small>
-                            </li>
-                        </ul>
+                            <ul class="additionals-wrapper">
+                                <li>
+                                    <i class="fa-solid fa-plus symbol-additional"></i>
+                                    <small>Molho Branco</small>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-plus symbol-additional"></i>
+                                    <small>Hamburguer</small>
+                                </li>
+                            </ul>
+                            <div class="obs-wrapper">
+                                <span>*Remover Alface</span>
+                            </div>
+                        </div>
+                        <div class="order-wrapper">
+                            <h5><span>2</span> Hamburguer</h5>
 
-                        <span>*Remover Alface</span>
+                            <ul class="additionals-wrapper">
+                                <li>
+                                    <i class="fa-solid fa-plus symbol-additional"></i>
+                                    <small>Molho Branco</small>
+                                </li>
+                                <li>
+                                    <i class="fa-solid fa-plus symbol-additional"></i>
+                                    <small>Hamburguer</small>
+                                </li>
+                            </ul>
+                            <div class="obs-wrapper">
+                                <span>*Remover Alface</span>
+                            </div>
+                        </div>
+
                     </li>
-                    <li>
-                        <h5><span>2</span> Hamburguer</h5>
 
-                        <ul class="additionals-wrapper">
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Molho Branco</small>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Hamburguer</small>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <h5><span>2</span> Hamburguer</h5>
-
-                        <ul class="additionals-wrapper">
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Molho Branco</small>
-                            </li>
-                            <li>
-                                <i class="fa-solid fa-plus"></i>
-                                <small>Hamburguer</small>
-                            </li>
-                        </ul>
-                    </li>
+                    
                 </ul>
             </div>
+            <div class="btn-comecar">
+                <button>Começar</button>
+            </div>
+        </div>
 
-            <button>Começar</button>
+        <div class="elements">
+            <div class="order-info">
+                <div class="position-number-wrapper">
+                    <span>22</span>
+
+                    <h4>#00004</h4>
+                </div>
+                <div class="user-time-wrapper">
+                    <p><i class="fa-solid fa-user"></i> Mart bocudo</p>
+                    <span><i class="fa-regular fa-clock"></i> 13m 24s</span>
+                </div>
+            </div>
+                
+            <div class="products-wrapper">
+                <ul>
+                    <li>
+                        <div class="order-wrapper">
+                            <h5><span>2</span> Lasanha</h5>
+
+                            <div class="obs-wrapper">
+                                <span>*Remover lasanha</span>
+                            </div>
+                        </div>
+
+                    </li>
+
+                    
+                </ul>
+            </div>
+            <div class="btn-comecar">
+                <button>Começar</button>
+            </div>
+        </div>
+
+        <div class="elements">
+            <div class="order-info">
+                <div class="position-number-wrapper">
+                    <span>17</span>
+
+                    <h4>#00005</h4>
+                </div>
+                <div class="user-time-wrapper">
+                    <p><i class="fa-solid fa-user"></i> Smigolzin</p>
+                    <span><i class="fa-regular fa-clock"></i> 16m 32s</span>
+                </div>
+            </div>
+
+            <div class="products-wrapper">
+                <ul>
+                    <li>
+                        <div class="order-wrapper">
+                            <h5><span>1</span> Big Smougol</h5>
+
+                            <ul class="additionals-wrapper">
+                                <li>
+                                    <i class="fa-solid fa-plus symbol-additional"></i>
+                                    <small>cheddar</small>
+                                </li>
+                                
+                            </ul>
+                            
+                        </div>
+
+                    </li>    
+                </ul>
+            </div>
+            <div class="btn-comecar">
+                <button>Começar</button>
+            </div>
         </div>
     </div>
+
 </section>
